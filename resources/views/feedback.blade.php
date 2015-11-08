66654d63834228c366396e66e8f4184444b25c5e
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2015-11-08 17:57:56 +0800

    Fix user problem 5

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -14,7 +14,7 @@
             <div class="form-group">
                 <label>Feedback</label>
                 <div class="form-control" id="summernote">
-                    Be honest.
+                    <h1>Your feedback here.</h2>
                 </div>
             </div>
             <div class="form-group">
